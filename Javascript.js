8639e2429e5755654b492531fd4b7414792d0d2c
fandymahesa-KTP <fandymahesa90@gmail.com>
2019-08-04 13:25:14 +0700

    test

--- Javascript.js
+++ Javascript.js
@@ -1,2 +1,31 @@
-const batam = 'belajar javascript dasar';
-console.log ('batam');
+const batam = 'belajar javascript dasar'
+console.log (batam)
+
+const A = 'saya belajar'
+const B = 'saya belajar 1'
+const C =`${A} ${B}` //jangan pakai petik tpi bektik
+console.log(C)
+
+const angkah1 = 10
+const angkah2 = 20 
+const hasil =(angkah1+angkah2)
+console.log(hasil)
+
+
+const E= true
+console.log(E)
+
+const data=['jeko','Budi','Cicak','Doni']
+console.log(data[2])
+
+var number = 5 
+var resutl = (number==4)
+var resutl = (number == 5 )
+var resutl = (number == 5 )
+console.log(resutl)
+
+const kalkulatorA= 100
+const kalkulatorB= 200
+const hasil1 = (kalkulatorA + kalkulatorB)
+console.log(hasil1)
+
